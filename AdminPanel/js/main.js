bff429fd26c838f7b1441b404e9097e44804e92c
omar_ali <omar.sayedahmed.ali@gmail.com>
2023-11-01 10:13:19 +0200

    Admin panel UI updated

--- AdminPanel/js/main.js
+++ AdminPanel/js/main.js
@@ -1,10 +1,22 @@
+// add hovered class to selected list item
 let list = document.querySelectorAll(".navigation li");
 
-function activeLink() {
+function activeTab() {
   list.forEach((item) => {
     item.classList.remove("hovered");
   });
   this.classList.add("hovered");
 }
 
-list.forEach((item) => item.addEventListener("mouseover", activeLink));
+list.forEach((item) => item.addEventListener("click", activeTab));
+
+// Menu Toggle
+const menuToggle = document.querySelector(".toggle");
+const navigation = document.querySelector(".navigation");
+const main = document.querySelector(".main");
+
+menuToggle.onclick = function () {
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};
